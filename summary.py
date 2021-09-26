407f36c06fc8e6df655eb3d5bc76c670cb1b30d1
mattheqd <91358934+mattheqd@users.noreply.github.com>
2021-09-26 00:44:29 -0700

    Add files via upload
    Added summary feature and function that cleans up API output. os library was taken out as problems arising from file location errors on my side.

--- summary.py
+++ summary.py
@@ -1,7 +1,26 @@
 import speech_recognition as sr
 import moviepy.editor as mp
+import requests
 
-clip = mp.VideoFileClip(r"sample2.MOV")
+def format_summary(summary:str) -> str:
+  r'''
+  Makes the file output from the summarization API readable for print format 
+  Replaces the \n in file with an actual new line character
+  Also gets rid of backslashes received by the summary received by the API
+  Also deletes id in the beginning of the summary received by the APi, and additional formatting at the end
+  '''
+  summary = summary.replace('\\n', '\n')
+  summary = summary.replace('\\', '')
+  pos = 0
+  while True:
+    pos +=1
+    if summary[pos:pos+6] == 'output':
+      break
+  return summary[pos+10:-4]
+
+filename = 'sample.mp4'
+
+clip = mp.VideoFileClip(filename)
 
 clip.audio.write_audiofile(r"converted.wav")
 
@@ -19,3 +38,24 @@ with open('recognized.txt',mode ='w') as file:
    file.write("\n")
    file.write(result)
    print("ready!")
+
+# summarize result
+r = requests.post(
+    "https://api.deepai.org/api/summarization",
+    files={
+        'text': open('tedtalk.txt', 'rb'),
+    },
+    headers={'api-key': '13064deb-c7f8-462a-86ef-fef35f0d04b9'}
+)
+
+with open('summarized.txt',mode = 'w') as file:
+  file.write('Summarized Speech:\n')
+  summarized = str(r.json())
+  file.write(summarized)
+
+with open('summarized.txt',mode = 'r') as file:
+  summary = str(file.readlines())
+  summary = format_summary(summary)
+  print(summary)
+
+  
